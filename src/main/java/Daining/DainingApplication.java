ed75c07a4a46cbf6c6a5150048b79dd27123d5c2
Safayet-Shafi <77004144+Safayet-Shafi@users.noreply.github.com>
2025-12-14 23:14:12 +0600

    test

---
+++ src/main/java/Daining/DainingApplication.java
@@ -0,0 +1,13 @@
+package Daining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Daining/DainingApplicationTests.java
@@ -0,0 +1,13 @@
+package Daining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
